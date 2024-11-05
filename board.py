e5bcaa5ad2ca9f30e26f930af86b8b40b04055a2
NaughtyChas <3030561972@qq.com>
2024-11-05 12:52:13 +0800

    Add word fill logic
    Now the board will get filled with random words only. I haven't add other contents to fill.

--- board.py
+++ board.py
@@ -1,10 +1,45 @@
 import curses
+import random
 
 class Board:
     def __init__(self, stdscr, size=7):
         self.stdscr = stdscr
         self.size = size
         self.board = [[' ' for _ in range(size)] for _ in range(size)]
+        self.words = [
+            "PYTHON", "CODE", "DEBUG", "ALGORITHM", "FUNCTION",
+            "VARIABLE", "LOOP", "CONDITION", "ARRAY", "STRING",
+            "COMPUTER", "PROGRAM", "LANGUAGE", "DEVELOPER", "SOFTWARE",
+            "HARDWARE", "NETWORK", "DATABASE", "SECURITY", "ENCRYPTION"
+        ]
+        self.fill_board()
+
+    def fill_board(self):
+        # Filter out words longer than the board size
+        valid_words = [word for word in self.words if len(word) <= self.size]
+
+        # Randomly select 3 words to place on the board
+        selected_words = random.sample(valid_words, 3)
+
+        # Randomly place words on the board
+        for word in selected_words:
+            placed = False
+            while not placed:
+                direction = random.choice(['H', 'V'])  # H: Horizontal, V: Vertical
+                if direction == 'H' and self.size - len(word) >= 0:
+                    row = random.randint(0, self.size - 1)
+                    col = random.randint(0, self.size - len(word))
+                    if all(self.board[row][col + i] == ' ' for i in range(len(word))):
+                        for i in range(len(word)):
+                            self.board[row][col + i] = word[i]
+                        placed = True
+                elif direction == 'V' and self.size - len(word) >= 0:
+                    row = random.randint(0, self.size - len(word))
+                    col = random.randint(0, self.size - 1)
+                    if all(self.board[row + i][col] == ' ' for i in range(len(word))):
+                        for i in range(len(word)):
+                            self.board[row + i][col] = word[i]
+                        placed = True
 
     def draw_board(self):
         self.stdscr.clear()
@@ -25,7 +60,7 @@ class Board:
                 if j == self.size - 1:
                     self.stdscr.addstr(y, x + 4, '+')
                 if i < self.size:
-                    self.stdscr.addstr(y + 1, x, '|   ')
+                    self.stdscr.addstr(y + 1, x, f'| {self.board[i][j]} ')
                 if i < self.size and j == self.size - 1:
                     self.stdscr.addstr(y + 1, x + 4, '|')
 
